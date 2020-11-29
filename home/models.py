ce13ba02bf811f6274e1656afe1ef31589c66226
ManasChandan <manaschandan79@gmail.com>
2020-11-29 18:48:58 +0530

    some changes

--- home/models.py
+++ home/models.py
@@ -4,6 +4,7 @@ from django.db import models
 class contact(models.Model):
     name = models.CharField(max_length=122)
     email = models.CharField(max_length=122)
+    email_new = models.EmailField
     phone = models.CharField(max_length=12)
     dsc = models.TextField()
     date = models.DateField()

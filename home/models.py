7275fb273f70bfdf103731c2894baa601c63ba26
ManasChandan <manaschandan79@gmail.com>
2020-11-29 17:31:43 +0530

    Basic Website Completed

--- home/models.py
+++ home/models.py
@@ -7,3 +7,6 @@ class contact(models.Model):
     phone = models.CharField(max_length=12)
     dsc = models.TextField()
     date = models.DateField()
+
+    def __str__(self):
+        return self.name
--- home/views.py
+++ home/views.py
@@ -1,12 +1,14 @@
 from django.shortcuts import render , HttpResponse
 from home import models
 from datetime import datetime
+from django.contrib import messages
 
 # Create your views here.
 def index(request):
     context = {
         "variable":"This is manas Chandan Behera"
     }
+    #messages.success(request, "This is a test Message")
     return render(request , "index.html" , context)
     # CDN - Content Delivery Network
     # return HttpResponse("This is my home page")
@@ -22,6 +24,7 @@ def contact(request):
         dsc = request.POST.get('dsc')
         cont = models.contact(name=name, email=email, phone=phone, dsc=dsc, date=datetime.today())
         cont.save()
+        messages.success(request, "Your form Has been Submitted . Thank You !")
     return render(request , "contact.html")
 
 def services(request):
